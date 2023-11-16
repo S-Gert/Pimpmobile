caacee84591a7064709304373fd7f9ad39fcdab8
S-Gert <gert.sillaots@eek.ee>
2023-11-16 14:04:34 +0200

    Teleopi kood mootoritele

--- Maincode/Maincode.ino
+++ Maincode/Maincode.ino
@@ -7,16 +7,36 @@ ServoControl servo(D6, D3);
 MotorControl motors(D9, D0);
 ReceiverData receiver(D8, D7);
 
+int forward_motor_pin = D4;
+int backward_motor_pin = D10;
+
 double ch1, ch2;
 
 void setup(){
   Serial.begin(9600);
+  pinMode(forward_motor_pin, INPUT);
+  pinMode(backward_motor_pin, INPUT);
 }
 
 void loop(){
-  ch1 = receiver.readChannel1();
-  ch2 = receiver.readChannel2();
-  servo.run(ch2, 45);
-  motors.run(ch1);
-  receiver.printValues(ch1, ch2);
+  // ch1 = receiver.readChannel1();
+  // ch2 = receiver.readChannel2();
+  // servo.run(ch2, 45);
+  // motors.run(ch1);
+  // receiver.printValues(ch1, ch2);
+  if (digitalRead(forward_motor_pin) == 1){
+  motors.teleopRun(1, 200, 100, 1);
+  } else if (digitalRead(backward_motor_pin) == 1){
+  motors.teleopRun(1, 200, 100, 0);
+  } else if (digitalRead(forward_motor_pin) == 0){
+  motors.teleopRun(0, 200, 100, 1);
+  } else if (digitalRead(backward_motor_pin) == 0){
+  motors.teleopRun(0, 200, 100, 0);
+  }
+  // if (digitalRead(input_pin) == 1){
+  //   Serial.println("HIGH");
+  // }
+  // // delay(3000);
+  // motors.teleopRun(0, 400, 50, 1);
+  // delay(3000);
 }
--- Maincode/Motorcontrol.h
+++ Maincode/Motorcontrol.h
@@ -6,9 +6,12 @@ class MotorControl {
   public:
     MotorControl(int analog_pin, int digital_pin);
     void run(int pwm_val);
+    void teleopRun(bool input, int max_pwm_speed, int rampup_delay, bool forward);
   private:
     int _analog_pin;
     int _digital_pin;
+    int  _rampup_increment;
+    int _current_pwm_speed;
 };
 
 #endif
